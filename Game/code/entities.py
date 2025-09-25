69936191db2db8137f25a2f746b7a0d28ff969ab
Justin Ooi <Justinooiyiery@gmail.com>
2025-09-26 00:10:38 +0800

    46min

---
+++ Game/code/entities.py
@@ -0,0 +1,31 @@
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, groups):
+        super().__init__(groups)
+        self.image = pygame.Surface((100,100))
+        self.image.fill('red')
+        self.rect = self.image.get_frect(topleft = pos)
+
+        self.direction = vector()
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+        input_vector = vector()
+        if keys[pygame.K_UP]:
+            input_vector.y -= 1
+        if keys[pygame.K_DOWN]:
+            input_vector.y += 1
+        if keys[pygame.K_LEFT]:
+            input_vector.x -= 1
+        if keys[pygame.K_RIGHT]:
+            input_vector.x += 1
+        self.direction = input_vector
+                       
+
+    def move(self, dt):
+        self.rect.center += self.direction * 250 * dt
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
--- Game/code/main.py
+++ Game/code/main.py
@@ -2,11 +2,19 @@ from settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join, dirname, abspath
 
+from sprites import Sprite
+from entities import Player
+from groups import AllSprites
+
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Mycomon')
+        self.clock = pygame.time.Clock()
+
+        # groups
+        self.all_sprites = AllSprites()
 
         self.import_assets()
         self.setup(self.tmx_maps['world'], 'house')
@@ -20,10 +28,15 @@ class Game:
 
     def setup(self, tmx_map, player_start_pos):
         for x,y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
-            print(x,y, surf)
+            Sprite((x * TITLE_SIZE, y * TITLE_SIZE), surf, self.all_sprites) 
+
+        for obj in tmx_map.get_layer_by_name('Entities'):
+            if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
+                self.player = Player((obj.x, obj.y), self.all_sprites)
 
     def run(self):
         while True:
+            dt = self.clock.tick() / 1000
             # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -31,6 +44,8 @@ class Game:
                     exit()
 
             # game logic
+            self.all_sprites.update(dt)
+            self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 
 if __name__ == '__main__':
