90dd083dca402b29c987b04f946f2e38c3b80b86
Justin Ooi <Justinooiyiery@gmail.com>
2025-09-30 14:18:23 +0800

    1hr48min
    completed player animation

--- Game/code/entities.py
+++ Game/code/entities.py
@@ -6,16 +6,33 @@ class Entity(pygame.sprite.Sprite):
 
         # graphics
         self.frame_index, self.frames = 0, frames
+        self.facing_direction = 'down'
+
+        # movement
+        self.direction = vector()
+        self.speed = 250
 
         # sprite setup
-        self.image = self.frames['down'][self.frame_index] 
+        self.image = self.frames[self.get_state()][self.frame_index] 
         self.rect = self.image.get_frect(center = pos)
+
+    def animate(self, dt):
+        self.frame_index += ANIMATION_SPEED * dt
+        self.image = self.frames[self.get_state()][int(self.frame_index % len(self.frames[self.get_state()]))]
+
+    def get_state(self):
+        # logic
+        moving = bool(self.direction)
+        if moving:
+            if self.direction.x != 0:
+                self.facing_direction = 'right' if self.direction.x > 0 else 'left'
+            if self.direction.y != 0:
+                self.facing_direction = 'down' if self.direction.y > 0 else 'up'
+        return f'{self.facing_direction}{'' if moving else '_idle'}'
 
 class Player(Entity):
     def __init__(self, pos, frames, groups):
         super().__init__(pos, frames, groups)
-
-        self.direction = vector()
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -32,8 +49,9 @@ class Player(Entity):
                        
 
     def move(self, dt):
-        self.rect.center += self.direction * 250 * dt
+        self.rect.center += self.direction * self.speed * dt
 
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
